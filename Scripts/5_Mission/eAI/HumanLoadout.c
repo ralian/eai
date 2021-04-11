677247c66e1f4935b7daf1495f6e976ff0372d49
mokdevel <mokdevel@hotmail.com>
2021-04-11 22:22:28 +0300

    Loadout changes. New logo.

--- Scripts/5_Mission/eAI/HumanLoadout.c
+++ Scripts/5_Mission/eAI/HumanLoadout.c
@@ -1,48 +1,72 @@
 class HumanLoadout {	
-	ref TStringArray Shirts = {"HikingJacket_Blue "};
+	ref TStringArray Shirts = {"HikingJacket_Blue"};
 	ref TStringArray Pants = {"SlacksPants_Blue"}; 							
 	ref TStringArray Shoes = {"HikingBootsLow_Blue"};			
 	ref TStringArray BackPacks = {"TaloonBag_Blue"};					
 	ref TStringArray Vests = {"PressVest_Blue"};		
-	ref TStringArray Headgear = {"BaseballCap_Blue "};
+	ref TStringArray Headgear = {"BaseballCap_Blue"};
 	ref TStringArray Gloves = {"SurgicalGloves_Blue"};	
 	ref TStringArray Misc = {"CivilianBelt"};																			
+	ref TIntArray	 ClothesHealth = {10,100}; 						//Item health given. 10%->100%
 	
-	ref TStringArray WeaponMelee = {"Pickaxe", "WoodAxe", "FirefighterAxe", "Shovel"}; 	
-	ref TStringArray WeaponRifle = {"M4A1", "AKM", "SVD", "M4A1"}; 	
+	ref TStringArray WeaponMelee = {"MeleeBat"}; 	
+	ref TStringArray WeaponRifle = {"Ruger1022"}; 	
 	ref TIntArray	 WeaponRifleMagCount = {1,3}; 	
-	ref TStringArray Loot = {"SodaCan_Cola"};  								//These are added always
-	ref TStringArray LootRandom = {"Rope", "Screwdriver"};  				//Added with a LootRandomChance%
-//	ref TIntSet	 	 LootRandomChance = {30};									//Add item from Loot array
-	ref TStringArray WeaponHandgun = {""}; 		
+	ref TStringArray WeaponHandgun = {"MakarovIJ70"}; 		
 	ref TIntArray	 WeaponHandgunMagCount = {1,3}; 	
-	
+	ref TIntArray	 WeaponHealth = {10,100}; 						//Weapon health given. 10%->100%
+
+	ref TStringArray Loot = {"SodaCan_Cola"};  						//These are added always
+	ref TStringArray LootRandom = {"Screwdriver"};  				//Added with a LootRandomChance%
+//	ref TIntSet	 	 LootRandomChance = {30};						//Add item from Loot array
+	ref TIntArray	 LootHealth = {10,100}; 						//Item health given. 10%->100%
+		
+	//---------------------
 	static string LoadoutSaveDir = "$profile:";
 	static string LoadoutDataDir = "eAI/Scripts/Data/Loadout/";
 	
 	static void Apply(PlayerBase h) {}
 
 	static void AddClothes(PlayerBase h, SoldierLoadout Loadout) {
-		h.GetInventory().CreateInInventory(Loadout.Pants.GetRandomElement());
-		h.GetInventory().CreateInInventory(Loadout.Shirts.GetRandomElement());
-		h.GetInventory().CreateInInventory(Loadout.Shoes.GetRandomElement());
-		h.GetInventory().CreateInInventory(Loadout.Headgear.GetRandomElement());
-		h.GetInventory().CreateInInventory(Loadout.Gloves.GetRandomElement());			
-		h.GetInventory().CreateInInventory(Loadout.BackPacks.GetRandomElement());
-		h.GetInventory().CreateInInventory(Loadout.Vests.GetRandomElement());
-		h.GetInventory().CreateInInventory(Loadout.Misc.GetRandomElement());
+		EntityAI item;
+		int minhealth = Loadout.ClothesHealth[0];
+		int maxhealth = Loadout.ClothesHealth[1];
+		float HealthModifier;
+		
+		item = h.GetInventory().CreateInInventory(Loadout.Pants.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+//			Print("HumanLoadout: Add pants: " + item.GetMaxHealth() + "/" + HealthModifier + "/" + item.GetMaxHealth() * HealthModifier + ")" );
+		item = h.GetInventory().CreateInInventory(Loadout.Shirts.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+		item = h.GetInventory().CreateInInventory(Loadout.Shoes.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+		item = h.GetInventory().CreateInInventory(Loadout.Headgear.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+		item = h.GetInventory().CreateInInventory(Loadout.Gloves.GetRandomElement());			
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+		item = h.GetInventory().CreateInInventory(Loadout.BackPacks.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+		item = h.GetInventory().CreateInInventory(Loadout.Vests.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
+		item = h.GetInventory().CreateInInventory(Loadout.Misc.GetRandomElement());
+			HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+			item.SetHealth(item.GetMaxHealth() * HealthModifier);
 
 		Print("HumanLoadout: Added clothes");
 	}
 		
-	static void AddWeapon(PlayerBase h, string weapon) {
+	static void AddWeapon(PlayerBase h, string weapon, int minhealth = 100, int maxhealth = 100) {
 		EntityAI gun = h.GetHumanInventory().CreateInHands(weapon);
-//		ItemBase itemBs;
-//		itemBs = ItemBase.Cast(gun);
-//        SetHealth(itemBs, 20);		
-//		gun.SetHealth("","",100);
-		gun.SetHealth(gun.GetMaxHealth()*0.5);
-		Print("HumanLoadout: Add weapon: " + weapon);
+		float HealthModifier = (Math.RandomInt(minhealth, maxhealth)) / 100;
+		gun.SetHealth(gun.GetMaxHealth() * HealthModifier);
+		Print("HumanLoadout: Add weapon: " + weapon + " (" + HealthModifier + ")" );
 	}
 	
 	static void AddMagazine(PlayerBase h, string weapon, int mincount, int maxcount = 0) {
@@ -120,7 +144,8 @@ class SoldierLoadout : HumanLoadout {
 		HumanLoadout.AddClothes(h, Loadout);
 
 		string weapon = Loadout.WeaponRifle.GetRandomElement();
-		HumanLoadout.AddWeapon(h, weapon);
+//		HumanLoadout.AddWeapon(h, weapon);
+		HumanLoadout.AddWeapon(h, weapon, Loadout.WeaponHealth[0], Loadout.WeaponHealth[1]);
 		HumanLoadout.AddMagazine(h, weapon, Loadout.WeaponRifleMagCount[0], Loadout.WeaponRifleMagCount[1]);
 	}
 }	
@@ -134,7 +159,8 @@ class PoliceLoadout : HumanLoadout {
 		HumanLoadout.AddClothes(h, Loadout);
 
 		string weapon = Loadout.WeaponRifle.GetRandomElement();
-		HumanLoadout.AddWeapon(h, weapon);
+//		HumanLoadout.AddWeapon(h, weapon);
+		HumanLoadout.AddWeapon(h, weapon, Loadout.WeaponHealth[0], Loadout.WeaponHealth[1]);
 		HumanLoadout.AddMagazine(h, weapon, 2);
 	}
 }
--- Scripts/5_Mission/eAI/eaiGame.c
+++ Scripts/5_Mission/eAI/eaiGame.c
@@ -77,7 +77,8 @@ class eAIGame {
 		
 		pb_AI.SetAI(ownerGrp);
 			
-		SoldierLoadout.Apply(pb_AI);	//or PoliceLoadout.Apply(pb_AI);
+//		SoldierLoadout.Apply(pb_AI);	//or 
+		PoliceLoadout.Apply(pb_AI);
 	}
 	
 	// Server Side: This RPC spawns a helper AI next to the player, and tells them to join the player's formation.
