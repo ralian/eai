0632dc49cf5f7d51dea6516646702d52377f0c63
Jacob-Mango <mangosprograms@gmail.com>
2021-05-05 21:44:04 +1000

    Fix for players sometimes spawning as AI

--- Scripts/3_Game/eAI/DayZGame.c
+++ Scripts/3_Game/eAI/DayZGame.c
@@ -13,12 +13,29 @@ modded class DayZGame
     void eAICreateManager()
     {
         // fancy way to spawn the manager from a submodule
-        m_eAI_Manager = eAIManagerBase.Cast("eAIManager".ToType().Spawn());
+        Class.CastTo(m_eAI_Manager, "eAIManager".ToType().Spawn());
     }
 
     eAIManagerBase eAIManagerGet()
     {
         return m_eAI_Manager;
+    }
+
+    override void GlobalsInit()
+    {
+        super.GlobalsInit();
+
+		string path = "cfgVehicles";
+		string child_name = "";
+        for (int i = m_CharClassNames.Count() - 1; i >= 0; i--)
+		{
+            child_name = m_CharClassNames[i];
+
+			if (child_name.Contains("eAI"))
+            {
+			    m_CharClassNames.RemoveOrdered(i);
+            }
+		}
     }
     
     override void OnUpdate(bool doSim, float timeslice)
